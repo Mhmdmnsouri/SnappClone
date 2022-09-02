a80ff9b5ba20fffbc746cff026c451ff970850ed
Mhmdmnsouri <bitshawcoder@gmail.com>
2022-09-03 00:05:47 +0430

    JavaScript code separation

---
+++ javaScript/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
